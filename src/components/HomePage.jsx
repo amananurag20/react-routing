a31d968855aa94fc7f7485a24899e79c0d0e4a60
Aman Anurag <aanurag@sensation.com>
2024-08-22 17:14:32 +0530

    key property

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,29 @@
-const HomePage = () => {
+import { useState } from "react";
+import {data} from "../utils/constant"
+
+const HomePage = () => { 
+
+   const [count, setCount]= useState(100);
+  console.log(data)
+ 
   return (
     <div>
       This is homeeee HomePage
+     
+      {count}
+      <button onClick={()=>setCount(count+100)} className="bg-blue-500">Increase counter</button>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp6hx9mmaps6rY5bJizCvWO2J-QrPCEpSqaw&s"
         alt="img"
       />
+
+      {data.map((car)=>{
+
+        return <div className="flex text-3xl font-semibold" key={Math.random()}>
+          <h1 className="bg-blue-950 mr-6">{car.model}</h1>
+          <p className="bg-green-500">{car.price}</p>
+        </div>
+      })}
     </div>
   );
 };
---
+++ src/utils/constant.js
@@ -0,0 +1,69 @@
+export const data=[
+    {
+      "model": "Sedan LX",
+      "make": "Eclipse Motors",
+      "year": 2022,
+      "price": 25000
+    },
+    {
+      "model": "SUV X5",
+      "make": "Terra Autos",
+      "year": 2023,
+      "price": 38000
+    },
+    {
+        "model": "SUV X5",
+        "make": "Terra Autos",
+        "year": 2023,
+        "price": 38000
+      },
+    {
+      "model": "Coupe GT",
+      "make": "Speedster Inc.",
+      "year": 2021,
+      "price": 32000
+    },
+    {
+      "model": "Hatchback 200",
+      "make": "MetroCar",
+      "year": 2020,
+      "price": 18000
+    },
+    {
+      "model": "Convertible Z4",
+      "make": "Sunline Vehicles",
+      "year": 2023,
+      "price": 45000
+    },
+    {
+      "model": "Sedan Elite",
+      "make": "Opulence Auto",
+      "year": 2022,
+      "price": 42000
+    },
+    {
+      "model": "SUV Ranger",
+      "make": "Offroad Dynamics",
+      "year": 2023,
+      "price": 50000
+    },
+    {
+      "model": "Truck Max",
+      "make": "HeavyDuty Motors",
+      "year": 2021,
+      "price": 35000
+    },
+    {
+      "model": "Electric E2",
+      "make": "FutureDrive",
+      "year": 2024,
+      "price": 48000
+    },
+    {
+      "model": "Hybrid Eco",
+      "make": "GreenMotion",
+      "year": 2023,
+      "price": 40000
+    }
+  ]
+
