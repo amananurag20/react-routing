12cc4830ad7a328f1b8415874f83b92acf054b06
Aman Anurag <aanurag@sensation.com>
2024-08-22 17:28:37 +0530

    dynamic key generation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
-import {data} from "../utils/constant"
+import { useEffect, useState } from "react";
+import {data} from "../utils/constant" ;
 
 const HomePage = () => { 
 
    const [count, setCount]= useState(100);
-  console.log(data)
+   const [newData, setNewData]= useState([]);
+  console.log(data);
+  console.log(crypto.randomUUID());
+   
+  useEffect(()=>{
+     const arr=data.map((car)=>{
+      return {...car,id:Math.random()}
+    });
+    setNewData(arr);
+  },[])
+ 
+console.log({newData})
  
   return (
     <div>
@@ -16,12 +27,12 @@ const HomePage = () => {
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp6hx9mmaps6rY5bJizCvWO2J-QrPCEpSqaw&s"
         alt="img"
       />
+       
+      {newData?.map((car)=>{
 
-      {data.map((car)=>{
-
-        return <div className="flex text-3xl font-semibold" key={Math.random()}>
-          <h1 className="bg-blue-950 mr-6">{car.model}</h1>
-          <p className="bg-green-500">{car.price}</p>
+        return <div className="flex text-3xl font-semibold" key={car?.id}>
+          <h1 className="bg-blue-950 mr-6">{car?.model}</h1>
+          <p className="bg-green-500">{car?.price}</p>
         </div>
       })}
     </div>
